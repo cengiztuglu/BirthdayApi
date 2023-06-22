2b1e6efd711abac1d96d805399f3d8e0bc22fe18
cengiztuglu <cngztuglu@gmail.com>
2023-06-22 17:15:15 +0300

    FirstApi

---
+++ BirthdayApi/Controllers/DavetiyeController.cs
@@ -0,0 +1,33 @@
+﻿using Birthdayy.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.UI.WebControls;
+
+namespace Birthdayy.Controllers
+{
+    public class DavetiyeController : ApiController
+    {
+        
+        public IEnumerable<DavetiyeModel> GetKatilanlar()
+        {
+            return Veritabani.Liste.Where(i => i.KatilmaDurumu == true);
+        }
+
+        public void PostEkle(DavetiyeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Veritabani.Add(model);
+
+
+            }
+        }
+    }
+}
---
+++ BirthdayApi/Models/Veritabani.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Birthdayy.Models
+{
+    public static class Veritabani
+    {
+        private static Dictionary<string, DavetiyeModel> _Liste;
+        static Veritabani()
+        {
+            _Liste = new Dictionary<string, DavetiyeModel>();
+            _Liste.Add("Hasan", new DavetiyeModel
+            {
+                Ad = "Hasan",
+                Eposta = "hasan@gmail.com",
+                KatilmaDurumu = true
+
+
+            });
+            _Liste.Add("Mehmet", new DavetiyeModel
+            {
+                Ad = "Mehmet",
+                Eposta = "mehmet@gmail.com",
+                KatilmaDurumu = false
+
+
+            });
+            _Liste.Add("Berin", new DavetiyeModel
+            {
+                Ad = "berrin",
+                Eposta = "mberiirn@gmail.com",
+                KatilmaDurumu = true
+
+
+            });
+
+        }
+        public static void Add(DavetiyeModel model)
+        {
+            string key = model.Ad.ToLower();
+            if (_Liste.ContainsKey(key))
+            {
+                _Liste[key] = model;
+            }
+            else
+            {
+                _Liste.Add(key, model);
+            }
+
+        }
+        public static IEnumerable<DavetiyeModel> Liste
+        {
+            get { return _Liste.Values; }
+        }
+    }
+}
